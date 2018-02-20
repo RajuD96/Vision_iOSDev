e4c765c8828c8c06140c1492594023c11a982645
RD <rajudhumne@gmail.com>
2018-02-20 15:30:51 +0530

    added tapGesture to capture image

--- Vision_iOSDev/Controller/CameraVC.swift
+++ Vision_iOSDev/Controller/CameraVC.swift
@@ -14,6 +14,7 @@ class CameraVC: UIViewController {
     var captureSession: AVCaptureSession!
     var cameraOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
+    var photoData: Data?
 
     @IBOutlet weak var cameraView: UIView!
     @IBOutlet weak var confidenceLbl: UILabel!
@@ -36,11 +37,12 @@ class CameraVC: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        let tap = UITapGestureRecognizer(target: self, action: #selector (didTapCameraView))
+        tap.numberOfTapsRequired = 1
         captureSession = AVCaptureSession()
         captureSession.sessionPreset = AVCaptureSession.Preset.hd1920x1080
         
         let backCamera = AVCaptureDevice.default(for: AVMediaType.video)
-        
         do {
             let input = try AVCaptureDeviceInput(device: backCamera!)
             if captureSession.canAddInput(input) == true {
@@ -57,19 +59,48 @@ class CameraVC: UIViewController {
                 previewLayer.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 
                 cameraView.layer.addSublayer(previewLayer!)
+                cameraView.addGestureRecognizer(tap)
                 captureSession.startRunning()
             }
-            
         }
         catch{
             debugPrint(error)
         }
-        
-        
+    }
+    
+    @objc func didTapCameraView() {
+        let settings = AVCapturePhotoSettings()
+        settings.previewPhotoFormat = settings.embeddedThumbnailPhotoFormat
+        cameraOutput.capturePhoto(with: settings, delegate: self)
         
     }
-
-   
+    
 
 }
+
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        if let error = error {
+            debugPrint(error)
+        }else {
+            photoData = photo.fileDataRepresentation()
+            let image = UIImage(data: photoData!)
+            self.capturedImageView.image = image
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
 
